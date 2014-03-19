c4f4b8e2c855ed0e17fdc3a523b69c5aacc0b9e1
Vince Panuccio <panuccio.vince@gmail.com>
2014-03-19 23:28:16 +1100

    Basic decompilation in to a textbox, in a single window.

---
+++ LinqPadSpy.Standalone/App.xaml.cs
@@ -0,0 +1,135 @@
+﻿namespace LinqPadSpy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition.Hosting;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
+    using System.Windows;
+    using System.Windows.Threading;
+
+    using ICSharpCode.ILSpy.Debugger.Services;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Static Fields
+
+        internal static IList<ExceptionData> StartupExceptions = new List<ExceptionData>();
+
+        private static CompositionContainer compositionContainer;
+
+        #endregion
+
+        static App()
+        {
+            var catalog = new AggregateCatalog();
+            
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(App).Assembly));
+
+            // Don't use DirectoryCatalog, that causes problems if the plugins are from the Internet zone
+            // see http://stackoverflow.com/questions/8063841/mef-loading-plugins-from-a-network-shared-folder
+            string appPath = Path.GetDirectoryName(typeof(App).Module.FullyQualifiedName);
+
+            LoadAssemblyByShortName(catalog, "ILSpy");
+            LoadAssemblyByShortName(catalog, "ICSharpCode.AvalonEdit");
+
+            foreach (string plugin in Directory.GetFiles(appPath, "*.Plugin.dll"))
+            {
+                string shortName = Path.GetFileNameWithoutExtension(plugin);
+                try
+                {
+                    var asm = Assembly.Load(shortName);
+                    asm.GetTypes();
+                    catalog.Catalogs.Add(new AssemblyCatalog(asm));
+                }
+                catch (Exception ex)
+                {
+                    // Cannot show MessageBox here, because WPF would crash with a XamlParseException
+                    // Remember and show exceptions in text output, once MainWindow is properly initialized
+                    StartupExceptions.Add(new ExceptionData { Exception = ex, PluginName = shortName });
+                }
+            }
+
+            compositionContainer = new CompositionContainer(catalog);
+
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                AppDomain.CurrentDomain.UnhandledException += ShowErrorBox;
+                Dispatcher.CurrentDispatcher.UnhandledException += Dispatcher_UnhandledException;
+            }
+
+            try
+            {
+                DebuggerService.SetDebugger(compositionContainer.GetExport<IDebugger>());
+            }
+            catch
+            {
+                // unable to find a IDebugger
+            }
+        }
+
+        #region Constructors and Destructors
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public static CompositionContainer CompositionContainer
+        {
+            get
+            {
+                return compositionContainer;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static void Dispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Debug.WriteLine(e.Exception.ToString());
+            MessageBox.Show(e.Exception.ToString(), "Sorry, we crashed");
+            e.Handled = true;
+        }
+
+        private static void LoadAssemblyByShortName(AggregateCatalog catalog, string ilspy)
+        {
+            var ilspyasm = Assembly.Load(ilspy);
+            ilspyasm.GetTypes();
+            catalog.Catalogs.Add(new AssemblyCatalog(ilspyasm));
+        }
+
+        private static void ShowErrorBox(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            if (ex != null)
+            {
+                Debug.WriteLine(ex.ToString());
+                MessageBox.Show(ex.ToString(), "Sorry, we crashed");
+            }
+        }
+
+        #endregion
+
+        internal class ExceptionData
+        {
+            #region Fields
+
+            public Exception Exception;
+
+            public string PluginName;
+
+            #endregion
+        }
+    }
+}
---
+++ LinqPadSpy.Standalone/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿namespace LinqPadSpy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Media;
+
+    using ICSharpCode.ILSpy;
+    using ICSharpCode.ILSpy.TextView;
+    using ICSharpCode.ILSpy.TreeNodes;
+
+    using Mono.Cecil;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors and Destructors
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.Content = GetDecompilerTextView(LINQPad.LinqPadSpyExtensions.GetLastLinqPadQueryAssembly());
+        }
+
+        public static DecompilerTextView GetDecompilerTextView(string assemblyPath)
+        {
+            return GetDecompilerTextView(assemblyPath, Application.Current);
+        }
+
+        public static DecompilerTextView GetDecompilerTextView(string assemblyPath, System.Windows.Application currenApplication)
+        {
+            var loadedAssembly = LoadAssembly(assemblyPath, currenApplication);
+
+            var assemblyDefinition = GetAssemblyDefinition(loadedAssembly);
+
+            var mainModule = assemblyDefinition.MainModule;
+
+            var assemblyTreeNode = new AssemblyTreeNode(loadedAssembly);
+
+            var typesToDecompile = GetModuleTypes(mainModule, assemblyTreeNode);
+
+            var decompilerTextView = new DecompilerTextView(App.CompositionContainer, composeParts: true);
+
+            decompilerTextView.Decompile(new CSharpLanguage(), typesToDecompile, new DecompilationOptions());
+
+            return decompilerTextView;
+        }
+
+        static LoadedAssembly LoadAssembly(string assemblyPath, Application currenApplication)
+        {
+            var assemblyList = new AssemblyList("LINQPadList");
+
+            return new LoadedAssembly(assemblyList, assemblyPath, currenApplication);
+        }
+
+        static AssemblyDefinition GetAssemblyDefinition(LoadedAssembly loadedAssembly)
+        {
+            var asmDef = loadedAssembly.AssemblyDefinition;
+
+            if (asmDef == null)
+            {
+                throw new InvalidOperationException("Could not load for some reason.");
+            }
+
+            return asmDef;
+        }
+
+        static IEnumerable<TypeTreeNode> GetModuleTypes(ModuleDefinition mainModule, AssemblyTreeNode assemblyTreeNode)
+        {
+            return mainModule.Types.OrderBy(t => t.FullName).Select(type => new TypeTreeNode(type, assemblyTreeNode));
+        }
+
+        #endregion
+    }
+}
+namespace LINQPad
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Mime;
+    using System.Windows;
+
+    public static class LinqPadSpyExtensions
+    {
+        public static void DumpDasm(this object value)
+        {
+            if (value == null) return;
+
+            var linqpadQueryAssemblyPath = GetLastLinqPadQueryAssembly();
+
+            using (var decompilerTextView = LinqPadSpy.MainWindow.GetDecompilerTextView(linqpadQueryAssemblyPath, new Application()))
+            {
+                PanelManager.DisplayWpfElement(decompilerTextView, "Decompiled");
+            }
+        }
+
+        public static string GetLastLinqPadQueryAssembly()
+        {
+            var tempPath = Path.Combine(Path.GetTempPath(), "linqpad");
+
+            var linqpadDirectory = new DirectoryInfo(tempPath);
+
+            var latestDirectory =
+                linqpadDirectory.GetDirectories().OrderByDescending(dir => dir.LastWriteTime).First();
+
+            var latestQuery =
+                latestDirectory.GetFiles().OrderByDescending(file => file.LastWriteTimeUtc).First();
+
+            return latestQuery.FullName;
+        }
+    }
+}
