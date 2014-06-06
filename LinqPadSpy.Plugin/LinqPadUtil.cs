eeaa64832034b7adc48fc02ea1562c12ab23a69a
Vince Panuccio <panuccio.vince@gmail.com>
2014-06-06 21:52:07 +1000

    Cache LINQPad active tab lookup.

--- LinqPadSpy.Plugin/LinqPadUtil.cs
+++ LinqPadSpy.Plugin/LinqPadUtil.cs
@@ -13,34 +13,52 @@
     /// </summary>
     public static class LinqPadUtil
     {
+
+        static readonly Lazy<SelectionPattern> ActiveTabSelectionPattern = new Lazy<SelectionPattern>(
+            () =>
+            {
+                // The code is qute verbose and ugly but it was the only way I know to look up 
+                // the combo box selection without performing a deep traverse of the control tree (which takes far too long to be acceptable).
+                var aeDesktop = AutomationElement.RootElement;
+
+                var aeForm =
+                    aeDesktop.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.NameProperty, "LINQPad 4"));
+
+                if (aeForm == null)
+                {
+                    return null;
+                }
+
+                var windowPane = aeForm.FindAll(TreeScope.Children, Condition.TrueCondition)[0];
+
+                var windowPanes = windowPane.FindAll(TreeScope.Children, Condition.TrueCondition);
+
+                var rightOfSplitPane = windowPanes[1];
+
+                var tabPane = rightOfSplitPane.FindAll(TreeScope.Children, Condition.TrueCondition)[1];
+
+                return (SelectionPattern)tabPane.GetCurrentPattern(System.Windows.Automation.SelectionPattern.Pattern);
+            });
+
+        static SelectionPattern SelectedLinqPadTab
+        {
+            get
+            {
+                return ActiveTabSelectionPattern.Value;
+            }
+        }
+
         /// <summary>
         /// Gets the language which LINQPad is currently set to for the active tab.
         /// </summary>
         /// <returns></returns>
         public static Language GetLanguageForQuery()
         {
-            // The code is qute verbose and ugly but it was the only way I know to look up 
-            // the combo box selection without performing a deep traverse of the control tree (which takes far too long to be acceptable).
-            var aeDesktop = AutomationElement.RootElement;
-
-            var aeForm =
-                aeDesktop.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.NameProperty, "LINQPad 4"));
-
-            if (aeForm != null)
+            try
             {
-                try
+                if (SelectedLinqPadTab != null)
                 {
-                    var windowPane = aeForm.FindAll(TreeScope.Children, Condition.TrueCondition)[0];
-
-                    var windowPanes = windowPane.FindAll(TreeScope.Children, Condition.TrueCondition);
-
-                    var rightOfSplitPane = windowPanes[1];
-
-                    var tabPane = rightOfSplitPane.FindAll(TreeScope.Children, Condition.TrueCondition)[1];
-
-                    var selectedTabPattern = (SelectionPattern)tabPane.GetCurrentPattern(SelectionPattern.Pattern);
-
-                    var selectedTab = selectedTabPattern.Current.GetSelection()[0];
+                    var selectedTab = SelectedLinqPadTab.Current.GetSelection()[0];
 
                     var combo =
                         selectedTab.FindAll(TreeScope.Children, Condition.TrueCondition)[0].FindAll(
@@ -49,13 +67,13 @@
                                 TreeScope.Children,
                                 new PropertyCondition(AutomationElement.AutomationIdProperty, "cboLanguage"))[0];
 
-                    var selp = (SelectionPattern)combo.GetCurrentPattern(SelectionPattern.Pattern);
+                    var selp = (SelectionPattern)combo.GetCurrentPattern(System.Windows.Automation.SelectionPattern.Pattern);
 
                     if (selp.Current.GetSelection().First().Current.Name.Contains("VB")) return new VBLanguage();
-
                 }
-                catch { } // This line disgusts me.
             }
+            catch { } // This line disgusts me.
+
             return new CSharpLanguage();
         }
 
