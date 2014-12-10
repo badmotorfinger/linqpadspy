b0f0a4c75ae331139885eec69662e658613b335f
Vince Panuccio <panuccio.vince@gmail.com>
2014-12-10 20:57:00 +1100

    Merged upstream changes from ILSpy master.

--- LinqPadSpy.Plugin/Properties/AssemblyInfo.cs
+++ LinqPadSpy.Plugin/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LinqPadSpy.Plugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A plugin for LINQPad which displays compiler generated code.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LinqPadSpy.Plugin")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
